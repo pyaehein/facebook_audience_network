a53a699e63d54bb666e2ca21807452b9ed217da5
Matthew Hilleman <matt@kodiakcodeworks.com>
2021-10-08 00:25:19 -0500

    Fixed: iOS Banner now fills width

--- lib/ad/ad_banner.dart
+++ lib/ad/ad_banner.dart
@@ -107,20 +107,15 @@ class _FacebookBannerAdState extends State<FacebookBannerAd> with AutomaticKeepA
       return Container(
         height: containerHeight,
         color: Colors.transparent,
-        child: Container(
-          width: widget.bannerSize.width.toDouble(),
-          child: Center(
-            child: UiKitView(
-              viewType: BANNER_AD_CHANNEL,
-              onPlatformViewCreated: _onBannerAdViewCreated,
-              creationParams: <String, dynamic>{
-                "id": widget.placementId,
-                "width": widget.bannerSize.width,
-                "height": widget.bannerSize.height,
-              },
-              creationParamsCodec: StandardMessageCodec(),
-            ),
-          ),
+        child: UiKitView(
+          viewType: BANNER_AD_CHANNEL,
+          onPlatformViewCreated: _onBannerAdViewCreated,
+          creationParams: <String, dynamic>{
+            "id": widget.placementId,
+            "width": widget.bannerSize.width,
+            "height": widget.bannerSize.height,
+          },
+          creationParamsCodec: StandardMessageCodec(),
         ),
       );
     } else {
